4d4060ae2d236af5b9d542dcf9a4dd4076cde2e5
famarin <61315066+famarin@users.noreply.github.com>
2026-03-07 07:48:02 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import JordanLand from '../jordan-land.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <JordanLand />
+  </React.StrictMode>
+)
